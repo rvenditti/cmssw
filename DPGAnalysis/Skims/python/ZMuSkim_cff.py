e0bac67b83b7bcbb6aad3568cf20a78a00d5c5ed
rosamaria <rosamaria.venditti@gmail.com>
2017-03-14 13:26:38 +0100

    ZMuSkim proposal 2017 config file

--- DPGAnalysis/Skims/python/ZMuSkim_cff.py
+++ DPGAnalysis/Skims/python/ZMuSkim_cff.py
@@ -12,22 +12,24 @@ ZMuHLTFilter.HLTPaths = ["HLT_Mu*","HLT_IsoMu*","HLT_DoubleMu*"]
 # Get muons of needed quality for Zs
 looseMuonsForZMuSkim = cms.EDFilter("MuonSelector",
                              src = cms.InputTag("muons"),
-                             cut = cms.string('pt > 10 && abs(eta)<2.4 && isGlobalMuon = 1 && isTrackerMuon = 1 && abs(innerTrack().dxy)<2.0'),
+                             cut = cms.string('pt > 5 &&  abs(eta)<2.4'),
                              filter = cms.bool(True)                                
                              )
 
 tightMuonsForZMuSkim = cms.EDFilter("MuonSelector",
                              src = cms.InputTag("looseMuonsForZMuSkim"),
-                             cut = cms.string('pt > 20'),
+                             cut = cms.string('pt > 25 &&  abs(eta)<2.4 && isGlobalMuon = 1 && isTrackerMuon = 1 && abs(innerTrack().dxy)<2.0 && (globalTrack().normalizedChi2() < 10) && (innerTrack().hitPattern().numberOfValidHits() > 10) && (isolationR03().sumPt/pt)<0.4'),
                              filter = cms.bool(True)                                
                              )
 
 # build Z-> MuMu candidates
 dimuonsZMuSkim = cms.EDProducer("CandViewShallowCloneCombiner",
                          checkCharge = cms.bool(False),
-                         cut = cms.string('mass > 30'),
+                         cut = cms.string('mass > 60 && mass < 120 && charge=0'),
                          decay = cms.string("tightMuonsForZMuSkim looseMuonsForZMuSkim")
                          )
+
+
 
 # Z filter
 dimuonsFilterZMuSkim = cms.EDFilter("CandViewCountFilter",
@@ -35,10 +37,35 @@ dimuonsFilterZMuSkim = cms.EDFilter("CandViewCountFilter",
                              minNumber = cms.uint32(1)
                              )
 
+
+
+#InitialPlots = cms.EDAnalyzer('RecoMuonAnalyzer',
+#                                   muonsInputTag = cms.InputTag("muons"),                                   )
+#PlotsAfterTrigger = cms.EDAnalyzer('RecoMuonAnalyzer',
+#                                   muonsInputTag = cms.InputTag("muons"),                                   )
+#PlotsAfterLooseMuon = cms.EDAnalyzer('RecoMuonAnalyzer',
+#                                   muonsInputTag = cms.InputTag("muons"),                                   )
+#PlotsAfterTightMuon = cms.EDAnalyzer('RecoMuonAnalyzer',
+#                                   muonsInputTag = cms.InputTag("muons"),
+#                                   )
+#PlotsAfterDiMuon = cms.EDAnalyzer('RecoMuonAnalyzer',
+#                                   muonsInputTag = cms.InputTag("muons"),
+#                                   )
+#TFileService = cms.Service("TFileService",
+#                                   fileName = cms.string("histoSingleMu_skim.root")#                                   )
+
+
+
 # Z Skim sequence
-diMuonSelSeq = cms.Sequence(ZMuHLTFilter *
+diMuonSelSeq = cms.Sequence(
+                            #InitialPlots *
+                            ZMuHLTFilter *
+                            #PlotsAfterTrigger *
                             looseMuonsForZMuSkim *
+                            #PlotsAfterLooseMuon  *
                             tightMuonsForZMuSkim *
+                            #PlotsAfterTightMuon *
                             dimuonsZMuSkim *
-                            dimuonsFilterZMuSkim
+                            dimuonsFilterZMuSkim 
+                            #PlotsAfterDiMuon
                             )
